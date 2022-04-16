26a105d0affa928a123e275a64c31f703dde128c
emilo85 <enrique.milo85@gmail.com>
2022-04-16 14:04:26 -0400

    adding redirection and error message to canActivate function

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { LoginService } from './login.service';
 
 @Injectable({
@@ -7,10 +8,12 @@ import { LoginService } from './login.service';
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor(public auth: LoginService) { }
+  constructor(public auth: LoginService, public router: Router,private toastr: ToastrService) { }
 
   canActivate(): boolean {
     if (!this.auth.isAuthenticated()) {
+      this.toastr.error('Sorry, you must be logged in to see our catalog!', 'Access denied');
+      this.router.navigate(['/']);
       return false;
     }
     return true;
