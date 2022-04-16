8ded41e4928e8f1a79a4c99f4de08848dadb1586
emilo85 <enrique.milo85@gmail.com>
2022-04-16 14:15:14 -0400

    adding properties to model video

--- src/app/models/video.ts
+++ src/app/models/video.ts
@@ -1,2 +1,17 @@
 export class Video {
+    
+    title:string;
+    description:string;
+    programType:string;
+    images:string;
+    releaseYear:string;
+
+    constructor(title:string, description:string, programType:string, images:string, releaseYear:string)
+    {
+        this.title = title;
+        this.description = description; 
+        this.programType = programType;
+        this.images = images;
+        this.releaseYear = releaseYear;
+    }   
 }
