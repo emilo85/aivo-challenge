cac00614d7106b92b88fafe36563a75aaec1697f
emilo85 <enrique.milo85@gmail.com>
2022-04-16 13:54:03 -0400

    creating component routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListVideosComponent } from './components/list-videos/list-videos.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'home', component: ListVideosComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
