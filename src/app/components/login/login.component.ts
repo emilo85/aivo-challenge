1cf26663aa2438a805aadc36316b44944f08bc1f
emilo85 <enrique.milo85@gmail.com>
2022-04-16 17:36:36 -0400

    adding log out function

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,17 +12,23 @@ export class LoginComponent implements OnInit {
 
   email: string;
   password: string;
+  user: string = ''
 
   constructor(public loginService: LoginService,private router: Router,private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if(currentUser != null){
+      this.user = currentUser.user.split('@')
+    }
+    else this.user = ''
+    
   }
 
   login() {
     this.loginService.login(this.email, this.password).subscribe(data => {
       
       if (data.access_token != '') {
-        console.log('datos:',data)
         localStorage.setItem('currentUser', JSON.stringify({ token: data.access_token, user: this.email }));        
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
         let user = currentUser.user.split('@') 
@@ -36,6 +42,11 @@ export class LoginComponent implements OnInit {
         console.log(error)
         this.toastr.error('Username or password is incorrect', 'Login error');
       })
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.router.navigate(['/'])
   }
 
 }
