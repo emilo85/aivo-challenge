c9b9317fa2e5fddf1ae63cfbdf4d1f5cc1b07f3d
emilo85 <enrique.milo85@gmail.com>
2022-04-16 12:39:37 -0400

    adding components references to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ListVideosComponent } from './components/list-videos/list-videos.component';
+import { VideoComponent } from './components/video/video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ListVideosComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,
