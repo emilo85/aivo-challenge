6e448e944eda6886e4552f249eb48ce06be8ee50
emilo85 <enrique.milo85@gmail.com>
2022-04-16 13:13:00 -0400

    adding bootstrap, flex-layout and ngx-toastr to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ListVideosComponent } from './components/list-videos/list-videos.component';
 import { VideoComponent } from './components/video/video.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { VideoComponent } from './components/video/video.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
