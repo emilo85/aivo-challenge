25e3892bdd39236ac05be870c52a95a3d6d90e09
emilo85 <enrique.milo85@gmail.com>
2022-04-19 14:06:06 -0400

    updating app.module with new components references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 import { ListVideosComponent } from './components/list-videos/list-videos.component';
 import { VideoComponent } from './components/video/video.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -11,14 +10,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { FormsModule } from '@angular/forms';
 import { FilterPipe } from './pipes/filter.pipe';
+import {AuthModule} from '@auth0/auth0-angular';
+import { LoginAuth0Component } from './components/login-auth0/login-auth0.component';
+import { LogoutAuth0Component } from './components/logout-auth0/logout-auth0.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     ListVideosComponent,
     VideoComponent,
-    FilterPipe
+    FilterPipe,
+    LoginAuth0Component,
+    LogoutAuth0Component,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +33,10 @@ import { FilterPipe } from './pipes/filter.pipe';
     ToastrModule.forRoot(),
     FormsModule,
     HttpClientModule,
+    AuthModule.forRoot({
+      "domain": "dev-yvbsy7qs.us.auth0.com",
+      "clientId": "lzVQv9V5ws8UNcFEXWjdlCLyHLhdYSxG"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
