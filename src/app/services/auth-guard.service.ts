c2b3d5c1ae2a438c78b43f65eef8f878c6f8a2d8
emilo85 <enrique.milo85@gmail.com>
2022-04-19 14:05:27 -0400

    updating guard service

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 import { ToastrService } from 'ngx-toastr';
-import { LoginService } from './login.service';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor(public auth: LoginService, public router: Router,private toastr: ToastrService) { }
+  constructor(public router: Router,private toastr: ToastrService, public auth0: AuthService) { }
 
-  canActivate(): boolean {
-    if (!this.auth.isAuthenticated()) {
-      this.toastr.error('Sorry, you must be logged in to see our catalog!', 'Access denied');
-      this.router.navigate(['/']);
-      return false;
-    }
-    return true;
+  canActivate(): Observable<boolean> | boolean {
+    console.log("start")
+    return this.auth0.isAuthenticated$.pipe(
+      tap(loggedIn => {
+        console.log("loggedIn",loggedIn)
+        if (!loggedIn) {
+          this.toastr.error('Sorry, you must be logged in to see our catalog!', 'Access denied');
+          this.router.navigate(['/']);
+        }
+      })
+    );
   }
 }
