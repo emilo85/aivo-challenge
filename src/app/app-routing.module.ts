de6fc26312fb32f510600503ec19c6a2f09630ff
emilo85 <enrique.milo85@gmail.com>
2022-04-19 14:06:40 -0400

    updating routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListVideosComponent } from './components/list-videos/list-videos.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginAuth0Component } from './components/login-auth0/login-auth0.component';
 import { AuthGuardService as AuthGuard } from './services/auth-guard.service';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
+  {path: '', component: LoginAuth0Component},
   {path: 'home', component: ListVideosComponent, canActivate: [AuthGuard]},
   {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
